6632c25fd940c9fcdad1b4f533b63e66806adada
kulikov.a <28kulikov@mail.ru>
2022-04-15 16:17:23 +0300

    debugging get_mail, documentation

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from windows import StartWindow
+from get_pdf import get_payments
 from get_mail import get_email_payments
 
 
-def get_file_payments():
-    get_email_payments()
-
-
 if __name__ == '__main__':
-    # get_file_payments()
-    start_window = StartWindow()
-    start_window.run()
+	get_email_payments()
+	results = get_payments()
+	start_window = StartWindow(results)
+	start_window.run()
--- venv/Scripts/log2design.py
+++ venv/Scripts/log2design.py
@@ -1,4 +1,4 @@
-#!C:\Users\kulikov.a\PycharmProjects\GitHub\System-sale-GUI\venv\Scripts\python.exe
+#!C:\Users\kulikov.a\PycharmProjects\System-sale-GUI\venv\Scripts\python.exe
 # Create Lyman/Fitz style long flat Design Files from plain-text onset files
 # EKK / June 2015
 # Python 2/3 compatibile, depends on Pandas and Numpy/Scipy
